d0dfa68c23773ea9a98733a11ce96a481a90522b
Jennifer M. Hays <jmh5sf@virginia.edu>
2018-09-28 10:50:07 -0400

    Update travis builds to include devel branch.

--- ci_scripts/run_ebmetad.sh
+++ ci_scripts/run_ebmetad.sh
@@ -5,7 +5,11 @@ pushd $HOME
  [ -d run_ebmetad ] || git clone --depth=1 --no-single-branch https://github.com/jmhays/run_ebmetad.git
  pushd run_ebmetad
   git branch -a
-  git checkout master
+  if [ "${TRAVIS_BRANCH}" != "master" ] ; then
+   git checkout devel
+  else
+   git checkout master
+  fi
   $PYTHON setup.py install
   PYTHONPATH=../sample_restraint/build/src/pythonmodule $PYTHON -m pytest --cov=./src
  popd
